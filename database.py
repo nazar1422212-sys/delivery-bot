8c41187a4dee20e7df59f3b03658dd41508b55d2
nazar1411112-prog <nazar1411112@gmail.com>
2026-06-15 18:16:12 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,26 @@
 import asyncpg
 from config import DATABASE_URL
+import asyncpg
+from config import DATABASE_URL
+
+async def connect_db():
+    global pool
+    pool = await asyncpg.create_pool(DATABASE_URL)
+
+async def execute(query, *args):
+    async with pool.acquire() as conn:
+        return await conn.execute(query, *args)
+
+async def fetch(query, *args):
+    async with pool.acquire() as conn:
+        return await conn.fetch(query, *args)
+
+# Функции для курьеров
+async def get_verified_couriers():
+    return await fetch("SELECT tg_id FROM couriers WHERE is_verified = TRUE AND online = TRUE")
+
+async def update_courier_verification(tg_id, status: bool):
+    await execute("UPDATE couriers SET is_verified = $1 WHERE tg_id = $2", status, tg_id)
 
 pool = None
 
