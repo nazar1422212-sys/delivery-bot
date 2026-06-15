8960fb5e4280504ad07c614e4e8dfc5f17b8b64d
nazar1411112-prog <nazar1411112@gmail.com>
2026-06-15 17:31:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
-DATABASE_URL = os.getenv("DATABASE_URL")
+TOKEN = os.getenv("8637372035:AAE1rq1Qx_WEIW8NpmYqvoCEu4Pqb8cnRgQ")
+DATABASE_URL = os.getenv("postgresql://nxxxctrl_user:kSlmM9yWn8TE1GEwNsIgeD7z0CFKghfX@dpg-d8o0lcreo5us738g3e10-a/nxxxctrlL")
 
 PRICE_PER_KM = 10
