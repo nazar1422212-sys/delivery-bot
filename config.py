94ce2909b3f40f9fd5f6f640839cab162a9a1d8b
nazar1411112-prog <nazar1411112@gmail.com>
2026-06-15 18:22:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-TOKEN = os.getenv("TOKEN")  # Задайте в настройках сервера
+TOKEN = os.getenv("TOKEN") 
 DATABASE_URL = os.getenv("DATABASE_URL")
-ADMIN_ID = [ 8800119191]
-PRICE_PER_KM = 10.0  # Цена в леях за км
+
+PRICE_PER_KM = 10
+ADMIN_ID = 8800119191 # Ваш ID без кавычек и списка
