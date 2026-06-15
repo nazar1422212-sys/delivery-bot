ba7a51c699f20df7f718af4f3f9bc1a88bc334f1
nazar1411112-prog <nazar1411112@gmail.com>
2026-06-15 23:38:47 +0300

    Implement HEAD request support in keep_alive.py
    Added support for HEAD requests in the home route.

--- keep_alive.py
+++ keep_alive.py
@@ -1,6 +1,11 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
 
+# В вашем файле keep_alive.py добавьте поддержку HEAD:
+@app.route('/', methods=['GET', 'HEAD'])
+def home():
+    return "Bot is running", 200
+
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
