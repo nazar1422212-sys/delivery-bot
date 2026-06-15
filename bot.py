f90cffce9d3e528774342d0ddfaa109d2ede22a9
nazar1411112-prog <nazar1411112@gmail.com>
2026-06-15 17:56:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,31 @@ from aiogram import Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 from keep_alive import run_web
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+class OrderForm(StatesGroup):
+    pickup = State()
+    delivery = State()
+
+@dp.message(Command("order"))
+async def start_order(message: Message, state: FSMContext):
+    await message.answer("Отправьте геолокацию точки А (посадка).")
+    await state.set_state(OrderForm.pickup)
+
+@dp.message(OrderForm.pickup)
+async def process_pickup(message: Message, state: FSMContext):
+    # Здесь должна быть логика сохранения координат
+    await state.update_data(pickup=message.location)
+    await message.answer("Теперь отправьте геолокацию точки Б (доставка).")
+    await state.set_state(OrderForm.delivery)
+
+@dp.message(OrderForm.delivery)
+async def process_delivery(message: Message, state: FSMContext):
+    data = await state.get_data()
+    # Логика расчета расстояния и вызова create_order
+    await message.answer("Заказ создан! Стоимость рассчитана в леях.")
+    await state.clear()
 
 run_web()
 
