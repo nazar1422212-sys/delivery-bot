aa2a194908041d22b2f3d928e9099c8d5116b928
nazar1411112-prog <nazar1411112@gmail.com>
2026-06-15 17:39:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
-TOKEN = os.getenv("8637372035:AAE1rq1Qx_WEIW8NpmYqvoCEu4Pqb8cnRgQ")
-DATABASE_URL = os.getenv("postgresql://nxxxctrl_user:kSlmM9yWn8TE1GEwNsIgeD7z0CFKghfX@dpg-d8o0lcreo5us738g3e10-a/nxxxctrlL")
-
-PRICE_PER_KM = 10
+TOKEN = os.getenv("TOKEN")  # Задайте в настройках сервера
+DATABASE_URL = os.getenv("DATABASE_URL")
+PRICE_PER_KM = 10.0  # Цена в леях за км
